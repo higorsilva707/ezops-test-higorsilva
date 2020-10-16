97a7a74804b5711ee3dac6dd7cc8fa50fba365f8
higorsilva707 <higuitosilva707@gmail.com>
2020-10-16 12:41:15 -0300

    Repair mongo

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 require('dotenv/config');
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
-const Message = mongoose.model('Message', {
-  name: String,
-  message: String
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
 })
 
+/*
 const {
   MONGO_USERNAME,
   MONGO_PASSWORD,
@@ -23,48 +24,58 @@ const {
   MONGO_DB
 } = process.env;
 
-const dbUrl = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
+const dbUrl = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?retryWrites=true&w=majority`;
+console.log(dbUrl);
+*/
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   connectTimeoutMS: 10000,
 };
 
+
+//var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+var dbUrl = 'mongodb+srv://admin:25081963a@cluster-ezops.aivkn.mongodb.net/<simple-chat>?retryWrites=true&w=majority'
+
 app.get('/messages', (req, res) => {
-  Message.find({}, (err, messages) => {
+  Message.find({},(err, messages)=> {
     res.send(messages);
   })
 })
+
+
+
 
 app.get('/messages/:user', (req, res) => {
-  const user = req.params.user
-  Message.find({ name: user }, (err, messages) => {
+  var user = req.params.user
+  Message.find({name: user},(err, messages)=> {
     res.send(messages);
   })
 })
 
+
 app.post('/messages', async (req, res) => {
-  try {
-    const message = new Message(req.body);
+  try{
+    var message = new Message(req.body);
 
-    const savedMessage = await message.save()
-    console.log('saved');
+    var savedMessage = await message.save()
+      console.log('saved');
 
-    const censored = await Message.findOne({ message: 'badword' });
-    if (censored)
-      await Message.remove({ _id: censored.id })
-    else
-      io.emit('message', req.body);
-    res.sendStatus(200);
+    var censored = await Message.findOne({message:'badword'});
+      if(censored)
+        await Message.remove({_id: censored.id})
+      else
+        io.emit('message', req.body);
+      res.sendStatus(200);
   }
-  catch (error) {
+  catch (error){
     res.sendStatus(500);
-    return console.log('error', error);
+    return console.log('error',error);
   }
-  finally {
+  finally{
     console.log('Message Posted')
   }
+
 })
 
 app.post('/messages/clear', (req, res) => {
@@ -73,18 +84,22 @@ app.post('/messages/clear', (req, res) => {
   })
 })
 
-io.on('connection', () => {
-  console.log(`a user is connected`)
+
+io.on('connection', () =>{
+  console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl, options, (err) => {
-  console.log('MongoDB is connected')
-
-  if (err) {
-    throw new Error(err)
-  }
+/*
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+  console.log('mongodb connected',err);
 })
+*/
+mongoose.connect(dbUrl, options)
+  .then(() => console.log('MongoDB is connected'))
+  .catch((error) => {
+    console.error(error)
+  })
 
-const server = http.listen(3000, () => {
-  console.log('Server is running on port', server.address().port);
+var server = http.listen(3000, () => {
+  console.log('server is running on port', server.address().port);
 });
